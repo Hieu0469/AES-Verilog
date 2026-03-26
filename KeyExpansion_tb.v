40a133f413cff7dc98adf5d14167a0e91521d205
Coder0469 <youremail@example.com>
2026-03-26 15:16:59 +0700

    Commit

--- KeyExpansion_tb.v
+++ KeyExpansion_tb.v
@@ -1,10 +1,11 @@
 module key_expansion_tb();
-    wire [127:0] key_1,key_2,key_3,key_4,key_5,key_6,key_7,key_8,key_9,key_10,key_11,key_12,key_13,key_14;
+    wire [127:0] key_0,key_1,key_2,key_3,key_4,key_5,key_6,key_7,key_8,key_9,key_10,key_11,key_12,key_13,key_14;
     reg [255:0] key;
     reg clk;
 
     key_expansion ke(
         .key(key),
+        .key_0(key_0),
         .key_1(key_1),
         .key_2(key_2),
         .key_3(key_3),
@@ -29,8 +30,9 @@ initial begin
 end
 
 initial begin
-    key = 256'h000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f;
+    key = 256'h603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4;
     #160; // Wait for the key expansion to complete
+    $display("Key 0: %h", key_0);
     $display("Key 1: %h", key_1);
     $display("Key 2: %h", key_2);
     $display("Key 3: %h", key_3);
@@ -45,5 +47,6 @@ initial begin
     $display("Key 12: %h", key_12);
     $display("Key 13: %h", key_13);
     $display("Key 14: %h", key_14);
+
 end
 endmodule
